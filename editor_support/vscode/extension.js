50aabf60745da02084ac3b9e31363f08f72d1840
tw4 <88425310+tw4@users.noreply.github.com>
2025-08-26 12:17:33 +0300

    feat: lsp support

--- editor_support/vscode/extension.js
+++ editor_support/vscode/extension.js
@@ -1,11 +1,63 @@
 // Nova Language Extension for VS Code
-const vscode = require('vscode');
+const path = require('path');
+const { workspace, ExtensionContext } = require('vscode');
+
+const {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} = require('vscode-languageclient/node');
+
+let client;
 
 function activate(context) {
     console.log('Nova Language Extension is now active!');
+
+    // Server executable path
+    const serverModule = context.asAbsolutePath(
+        path.join('server', 'out', 'server.js')
+    );
+
+    // Debug options for the server
+    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+    // Server options
+    const serverOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions
+        }
+    };
+
+    // Client options
+    const clientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'nova' }],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+
+    // Create and start the language client
+    client = new LanguageClient(
+        'novaLanguageServer',
+        'Nova Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client (also launches the server)
+    client.start();
 }
 
-function deactivate() {}
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
 
 module.exports = {
     activate,
