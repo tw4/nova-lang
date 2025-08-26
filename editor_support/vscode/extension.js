1e750d3be608a312cf2aa54325d4fc71f902a240
tw4 <88425310+tw4@users.noreply.github.com>
2025-08-26 12:00:02 +0300

    feat: support cscode nova language Extension by tw4

---
+++ editor_support/vscode/extension.js
@@ -0,0 +1,13 @@
+// Nova Language Extension for VS Code
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log('Nova Language Extension is now active!');
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
